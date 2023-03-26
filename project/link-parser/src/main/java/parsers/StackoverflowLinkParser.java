d2249962b4d939fdb087114a84f0dd03aab210c9
jizapika <lyovoch@bk.ru>
2023-03-26 14:35:37 +0300

    check git-user

--- project/link-parser/src/main/java/parsers/StackoverflowLinkParser.java
+++ project/link-parser/src/main/java/parsers/StackoverflowLinkParser.java
@@ -5,7 +5,7 @@ import answers.*;
 public class StackoverflowLinkParser extends AbstractParser {
     @Override
     public LinkParserAnswer parse(String link) {
-        String[] arr = link.split("/");
+        String[] arr =  link.split("/");
         if (arr.length == 4 && (link.startsWith("https://github.com/") || link.startsWith("https://www.github.com/"))) {
             try {
                 int id = Integer.parseInt(arr[3]);
